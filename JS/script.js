45db49af0312d865cd20ee659960a8608797ee73
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-27 21:18:21 +0300

    Add the toggle effect to the what we do section

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+
+/*let number = 7
+alert("this is a whole number"); */
+
+
+jQuery(".design").hover(function() {
+        jQuery("#show-design").toggle();
+
+ })
+
+ jQuery(".development").hover(function() {
+    jQuery("#show-development").toggle();
+
+})
+
+jQuery(".productmanagement").hover(function() {
+    jQuery("#show-productmanagement").toggle();
+
+})
